385c93cee6e7d6f1cf6124bf4e9f54af3e682071
Chell L. <pumpkinbiscuit@protonmail.com>
2024-02-14 00:55:19 -0500

    progress on parsing!

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,8 @@
+use std::collections::HashMap;
+use lazy_static::lazy_static; // For static HashMap initialization
+
 mod lexer;
+use lexer::{Token, Keyword};
 
 enum Kind {
     Code,
@@ -6,8 +10,9 @@ enum Kind {
 }
 
 enum Content {
-    Expr(Expr),
+    Keyword(Keyword),
     Atom(String),
+    Expr(Expr),
 }
 
 struct Expr {
@@ -15,8 +20,74 @@ struct Expr {
     content: Vec<Content>,
 }
 
+lazy_static! {
+    static ref STR_TO_KW: HashMap<&'static str, Keyword> = {
+        let mut m = HashMap::new();
+        m.insert("quote", Keyword::Quote);
+        m.insert("atom", Keyword::Atom);
+        m.insert("eq", Keyword::Eq);
+        m.insert("car", Keyword::Car);
+        m.insert("cdr", Keyword::Cdr);
+        m.insert("cons", Keyword::Cons);
+        m.insert("cond", Keyword::Cond);
+        m
+    };
+}
+
+fn parse_expr(tokens: &[Token]) -> (usize, Expr) {
+    println!("{:?}", tokens);
+
+    let mut result = Expr {
+        kind: Kind::Code,
+        content: vec![],
+    };
+
+    let len = tokens.len();
+    let mut i = 0;
+    while i < len {
+        match &tokens[i] {
+            // _ => println!("{:?}", &tokens[i]),
+            Token::LeftParen => {
+                let (y, expr) = parse_expr(&tokens[i..]);
+                i += y;
+                result.content.push(Content::Expr(expr));
+            },
+            Token::RightParen => {
+                break
+            },
+            Token::Quote => result.kind = Kind::Data,
+            Token::Atom(s) => {},
+            Token::KW(kw) => {},
+        }
+    }
+
+    let i = 0;
+    let expr = Expr {
+        kind: Kind::Code,
+        content: vec![],
+    };
+    (i, expr)
+}
+
 pub fn parse(input: String) {
-    let lexed = lexer::lex(input);
-    println!("{lexed}");
+    let mut lexed = lexer::lex(input);
+    // println!("{:?}", lexed);
+
+    for token in lexed.iter_mut() {
+        if let Token::Atom(s) = token {
+            if let Some(kw) = STR_TO_KW.get(s.as_str()) {
+                *token = Token::KW(*kw)
+            }
+        }
+    }
+    println!("{:?}", lexed);
+
+    let mut result: Vec<Expr> = vec![];
+
+    let len = lexed.len();
+    let mut i = 0;
+    while i < len {
+        let (c, expr) = parse_expr(&lexed[i..]);
+    }
 }
 
--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,10 +1,23 @@
 use regex::Regex;
 
+#[derive(Debug, Clone, Copy)]
+pub enum Keyword {
+    Quote,
+    Atom,
+    Eq,
+    Car,
+    Cdr,
+    Cons,
+    Cond,
+}
+
 #[derive(Debug)]
 pub enum Token {
     LeftParen,
     RightParen,
+    Quote,
     Atom(String),
+    KW(Keyword),
 }
 
 pub fn lex(input: String) -> Vec<Token> {
@@ -12,6 +25,7 @@ pub fn lex(input: String) -> Vec<Token> {
 
     let re_leftparen = Regex::new(r"^\(").unwrap();
     let re_rightparen = Regex::new(r"^\)").unwrap();
+    let re_quote = Regex::new(r"^'").unwrap();
     let re_atom = Regex::new(r"^[a-z]+").unwrap();
     let re_ignore = Regex::new(r"^( |\n)+").unwrap();
 
@@ -19,24 +33,34 @@ pub fn lex(input: String) -> Vec<Token> {
     let mut i = 0;
     while i < len {
         match input {
-            _ if re_ignore.is_match(&input[i..]) => {
-                println!("IGNORE");
-                i += 1;
-            }
-            _ if re_atom.is_match(&input[i..]) => {
-                println!("ATOM");
-                let v = re_atom.find(&input[i..]).map(|s| s.as_str()).unwrap();
-                result.push(Token::Atom(String::from(v)));
-                i += v.len();
-            }
+            // (
             _ if re_leftparen.is_match(&input[i..]) => {
-                println!("LEFTPAREN");
+                // println!("LEFTPAREN");
                 result.push(Token::LeftParen);
                 i += 1;
             }
+            // )
             _ if re_rightparen.is_match(&input[i..]) => {
-                println!("RIGHTPAREN");
+                // println!("RIGHTPAREN");
                 result.push(Token::RightParen);
+                i += 1;
+            }
+            // '
+            _ if re_quote.is_match(&input[i..]) => {
+                // println!("QUOTE");
+                result.push(Token::Quote);
+                i += 1;
+            }
+            // Atom
+            _ if re_atom.is_match(&input[i..]) => {
+                let v = re_atom.find(&input[i..]).map(|s| s.as_str()).unwrap();
+                result.push(Token::Atom(String::from(v)));
+                i += v.len();
+                // println!("ATOM: {:?}, LEN: {:?}", v, v.len());
+            }
+            // Ignore
+            _ if re_ignore.is_match(&input[i..]) => {
+                // println!("IGNORE");
                 i += 1;
             }
             _ => {
